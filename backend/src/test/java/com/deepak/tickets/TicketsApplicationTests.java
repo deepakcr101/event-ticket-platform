7c5e090e716c8c10def1e792616f443c0b819099
deepak <piomega01@gmail.com>
2025-08-10 21:31:34 +0530

    backend bugs fixed

--- backend/src/test/java/com/deepak/tickets/TicketsApplicationTests.java
+++ backend/src/test/java/com/deepak/tickets/TicketsApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TicketsApplicationTests {
-
-  @Test
-  void contextLoads() {
-  }
-
+@ActiveProfiles("test")
+public class TicketsApplicationTests {
+    @Test
+    void contextLoads() {}
 }
+
