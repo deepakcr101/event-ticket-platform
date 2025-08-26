8025aa166308febcbd09e0fc15f35ccd2831a7b5
deepak <piomega01@gmail.com>
2025-08-27 03:12:11 +0530

    Some minor bugs fixed

--- backend/src/main/java/com/deepak/tickets/TicketsApplication.java
+++ backend/src/main/java/com/deepak/tickets/TicketsApplication.java
@@ -3,6 +3,8 @@ package com.deepak.tickets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class TicketsApplication {
 
